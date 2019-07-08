301b6a2561227ec312b9b1a4ca86fbb1d67ba5bc
Andrew <bg43179@gmail.com>
2019-07-08 14:11:00 -0700

    ac - Create a form for users to input comments

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import Header from './Header';
+import { Container, Row, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import Header from './Header.js';
+
+class DataForm extends React.Component {
+  render() {
+    return (
+      <Form >
+        <FormGroup>
+          <Label for="name">Name</Label>
+          <Input id='name' type="text" placeholder="Enter name" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="comments">Comments</Label>
+          <Input id="comments" type="textarea" placeholder="Enter Comments" />
+        </FormGroup>
+        <FormGroup>
+          <Button color="primary">Submit</Button>
+        </FormGroup>
+      </Form>
+    );
+  }
+}
 
 export default function App() {
   return (
-    <div>
-      <Header title="Tell us what you think" />
-      Put your components here: Flash Message, Form, Footer
-    </div>
+    <Container>
+      <Row>
+        <Col md={{ size: 6, offset: 3 }}>
+          <Header title="Tell us what you think" />
+          <DataForm />
+          <footer className='text-center text-muted' style={{ fontSize: '0.8em' }}>Copyright: Appfolio Inc. Onboarding</footer>
+        </Col>
+      </Row>
+    </Container>
+
+
   );
 }
 
--- app/javascript/image_sharing/test/components/App.spec.js
+++ app/javascript/image_sharing/test/components/App.spec.js
@@ -1,13 +1,33 @@
 /* eslint-env mocha */
 
 import assert from 'assert';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import React from 'react';
+import { Button, Input } from 'reactstrap';
 import App from '../../components/App';
 
 describe('<App />', () => {
   it('should render correctly', () => {
     const wrapper = mount(<App />);
     assert(wrapper.contains('Tell us what you think'));
+  });
+
+  it('should create a form for user to input name and comment', () => {
+    const wrapper = shallow(<App />);
+    const dataForm = wrapper.find('DataForm').dive();
+    assert.strictEqual(dataForm.find(Input).length, 2);
+    assert.strictEqual(dataForm.find(Input).at(0).prop('type'), 'text');
+    assert.strictEqual(dataForm.find(Input).at(1).prop('type'), 'textarea');
+  });
+
+  it('should create a form with button', () => {
+    const wrapper = shallow(<App />);
+    const dataForm = wrapper.find('DataForm').dive();
+    assert.strictEqual(dataForm.find(Button).prop('color'), 'primary');
+  });
+
+  it('should create a footer', () => {
+    const wrapper = shallow(<App />);
+    assert(wrapper.find('footer').contains('Copyright: Appfolio Inc. Onboarding'));
   });
 });
