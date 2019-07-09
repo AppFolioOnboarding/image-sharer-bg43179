ca3cb46d45ac645d8278c31b345d900bcfa4802d
Andrew <bg43179@gmail.com>
2019-07-09 15:49:21 -0700

    ac - add create method for api

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -1,7 +1,11 @@
 module Api
   class FeedbacksController < ApplicationController
     def create
-      # Implement your create action
+      if params[:name].present? && params[:comment].present?
+        render json: { text: 'success' }, status: :created
+      else
+        render json: { error: 'error' }, status: :unprocessable_entity
+      end
     end
   end
 end
